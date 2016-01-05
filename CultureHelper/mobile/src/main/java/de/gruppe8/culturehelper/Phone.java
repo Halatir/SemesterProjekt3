cb9bb1514368db86cbe9d076031d483d014ab77f
rominamarsico <romina.marsico@stud.h-da.de>
2016-01-05 14:38:07 +0100

    Location in der Phone.java Datei

--- CultureHelper/mobile/src/main/java/de/gruppe8/culturehelper/Phone.java
+++ CultureHelper/mobile/src/main/java/de/gruppe8/culturehelper/Phone.java
@@ -1,5 +1,7 @@
 package de.gruppe8.culturehelper;
 
+import android.location.Location;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -7,14 +9,22 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.wearable.PutDataMapRequest;
 import com.google.android.gms.wearable.Wearable;
 
 import java.util.Date;
 
-public class Phone extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+public class Phone extends AppCompatActivity implements
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener
+        LocationListener {
 
     private GoogleApiClient mGoogleApiClient;
     private static final String TAG = "PHONE";
@@ -25,22 +35,41 @@ public class Phone extends AppCompatActivity implements GoogleApiClient.Connecti
         setContentView(R.layout.activity_phone);
 
         //Erstelle Google API Client zur Kommunikation mit Wear
+        // ATTENTION: This "addApi(AppIndex.API)"was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
         mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(LocationServices.API)
                 .addApi(Wearable.API)
                 .addConnectionCallbacks(this)
                 .addOnConnectionFailedListener(this)
-                .build();
+                .addApi(AppIndex.API).build();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (mGoogleApiClient.isConnected()) {
+            LocationServices.FusedLocationApi
+                    .removeLocationUpdates(mGoogleApiClient, (LocationListener) this);
+        }
+        mGoogleApiClient.disconnect();
+    }
 
         //Testbutton zum Daten versenden
         Button button = (Button) findViewById(R.id.Testbutton);
         button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+
+        public void onClick(View v) {
                 pushStringsToWear();
             }
-        });
+        })
 
-    }
 
     //Sendet Daten an Wear, wenn aufgerufen  !!!!
     private void pushStringsToWear() {
@@ -64,7 +93,78 @@ public class Phone extends AppCompatActivity implements GoogleApiClient.Connecti
         Information.setText(a);
     }
 
-    //zu versendende Daten--->müssen anhand der Datenbank vor dem Absenden verändert werden
+    //---------------------- Request Location Updates ----------------------
+    @Override
+    public void onConnected(Bundle bundle) {
+        LocationRequest locationRequest = LocationRequest.create()
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY) //is more likely to use GPS than other priority values
+                .setInterval(60*1000)
+                .setFastestInterval(10*1000);
+
+        LocationServices.FusedLocationApi
+                .requestLocationUpdates(mGoogleApiClient, locationRequest, (LocationListener) this)
+                .setResultCallback(new ResultCallback() {
+
+                    @Override
+                    public void onResult(Result result) {
+                        Status status = result.getStatus();
+                        if (status.getStatus().isSuccess()) {
+                            if (Log.isLoggable(TAG, Log.DEBUG)) {
+                                Log.d(TAG, "Successfully requested location updates");
+                            }
+                        } else {
+                            Log.e(TAG,
+                                    "Failed in requesting location updates, "
+                                            + "status code: "
+                                            + status.getStatusCode()
+                                            + ", message: "
+                                            + status.getStatusMessage());
+                        }
+                    }
+                });
+    }
+
+
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        if (Log.isLoggable(TAG, Log.DEBUG)) {
+            Log.d(TAG, "connection to location client suspended");
+        }
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
+    }
+
+
+//zu versendende Daten--->müssen anhand der Datenbank vor dem Absenden verändert werden
     public static class Inhalt {
 
         public static  String NOTIFICATION_PATH = "/Warning";
@@ -81,15 +181,31 @@ public class Phone extends AppCompatActivity implements GoogleApiClient.Connecti
         super.onStart();
         mGoogleApiClient.connect();
         Log.d(TAG, "connected to APICLIENT" + new Date().getTime());
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Phone Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://de.gruppe8.culturehelper/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(mGoogleApiClient, viewAction);
     }
+
     @Override
     public void onConnected(Bundle bundle) {
         Log.d(TAG, "Connected");
     }
+
     @Override
     public void onConnectionSuspended(int i) {
         Log.d(TAG, "Connection Suspended");
     }
+
     @Override
     public void onConnectionFailed(ConnectionResult connectionResult) {
         Log.d(TAG, "Connection Failed");
@@ -99,6 +215,19 @@ public class Phone extends AppCompatActivity implements GoogleApiClient.Connecti
     @Override
     protected void onStop() {
         super.onStop();
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Phone Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://de.gruppe8.culturehelper/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(mGoogleApiClient, viewAction);
         mGoogleApiClient.disconnect();
         Log.d(TAG, "disconected from APICLIENT" + new Date().getTime());
     }
